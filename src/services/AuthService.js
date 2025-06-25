3f4099511ff84dee4385c0edd140029f0e4847bc
Võ Hoàng Tuấn <166472565+vhtuan2an@users.noreply.github.com>
2025-06-25 14:03:15 +0700

    fix: correct import path for JwtService in AuthService

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const { generalAccessToken } = require("./Jwtservice");
+const { generalAccessToken } = require("./JwtService");
 const User = require("../models/UserModel");
 const EmailService = require("../services/EmailService");
 
